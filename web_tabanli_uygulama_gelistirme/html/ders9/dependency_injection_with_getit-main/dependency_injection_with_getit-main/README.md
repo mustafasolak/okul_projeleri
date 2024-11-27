794bb6eb5386f8a791f77d8188eae13fb14f0fbb
Mustafa SOLAK <mustafasolak48@gmail.com>
2024-11-27 12:07:06 +0300

    Form2 dosyası eklendi
    Gereksiz flutter projesi silindi. Form örnekleri dosyası eklendi.

--- web_tabanli_uygulama_gelistirme/html/ders9/dependency_injection_with_getit-main/dependency_injection_with_getit-main/README.md
+++
@@ -1,2 +0,0 @@
-# dependency_injection_with_getit
- Dependency Injection with get_it
--- web_tabanli_uygulama_gelistirme/html/ders9/dependency_injection_with_getit-main/dependency_injection_with_getit-main/android/app/src/main/kotlin/com/example/dependency_injection_with_getit/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.dependency_injection_with_getit
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
