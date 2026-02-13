b15512df2d3eaf9c85c15e32a96c7f128f09be18
mustafasolak <mustafasolak48@gmail.com>
2026-02-13 09:18:13 +0300

    JS başlandı.

---
+++ web_tabanli_uygulama_gelistirme/2025-2026/javascript/hafta15/js/resim_galerisi.js
@@ -0,0 +1,15 @@
+function panda1(){
+    document.getElementById("resimGosterici").src = "resimler/panda.jpg";
+}
+
+function panda2(){
+    document.getElementById("resimGosterici").src = "resimler/panda2.jpg";
+}
+
+function doga(){
+    document.getElementById("resimGosterici").src = "resimler/doga.jpg";
+}
+
+function sonbahar(){
+    document.getElementById("resimGosterici").src = "resimler/sonbahar.jpg";
+}
---
+++ web_tabanli_uygulama_gelistirme/2025-2026/javascript/hafta16/degiskenler.js
@@ -0,0 +1,26 @@
+var isim = "Mustafa";
+var yas = 39;
+var piSayisi = 3.14;
+var odemeYapilmismi = false;
+// nesne türü 
+
+var ogrenciBilgi = {
+    adi : "Ali",
+    soyadi : "YILMAZ",
+    yas : 20,
+    okul : "ŞABÇ MTAL",
+    bolum : "Bilişim Teknolojileri",
+}
+
+
+document.writeln("<b>İsim :</b> " + isim + "<br>");
+document.writeln("<strong>Yaş :</strong> " + yas + "<br>");
+document.writeln("<b>Pi Sayısı :</b> " + piSayisi + "<br>");
+document.writeln("<strong>Ödeme Yapılmış mı? :</strong> " + odemeYapilmismi + "<br>");
+
+document.writeln("<b>Öğrenci Bilgileri :</b><br>");
+document.writeln("Adı : " + ogrenciBilgi.adi + "<br>");
+document.writeln("Soyadı : " + ogrenciBilgi.soyadi + "<br>");
+document.writeln("Okul : " + ogrenciBilgi.okul + "<br>");
+document.writeln("Bölüm : " + ogrenciBilgi.bolum + "<br>");
+document.writeln("Yaşı : " + ogrenciBilgi.yas + "<br>");
